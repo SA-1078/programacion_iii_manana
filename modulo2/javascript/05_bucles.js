f50820e024a5eae8ace32428459e6aa0c6d57010
unknown <santiagocolimba10@gmail.com>
2025-10-21 10:04:19 -0500

    Clase Martes 21 de Octubre del 2025: Archivos javascript nuevos 04_condicionales y 05_bucles

---
+++ modulo2/javascript/05_bucles.js
@@ -0,0 +1,15 @@
+for (let i=1; i <= 10; i++){
+    console.log("numero", i);
+}
+
+suma = 0;
+for (let i=1; i <=10; i++){
+    suma+=i;
+}
+console.log("Sumatoria indices", suma);
+
+
+nombre = "Santiago";
+for (let i=0; i <= nombre.length -1; i++){
+    console.log(nombre[i]);
+}
