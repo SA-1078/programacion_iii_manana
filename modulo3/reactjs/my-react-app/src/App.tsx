79b72912da0951bd7e2dc50eb77a2beec6785da6
unknown <santiagocolimba10@gmail.com>
2025-12-11 10:01:03 -0500

    Clase Jueves 11 de Diciembre de 2025, Avance en reactjs, carpeta nueva src- useState con varios archivos nuevos

--- modulo3/reactjs/my-react-app/src/App.tsx
+++ modulo3/reactjs/my-react-app/src/App.tsx
@@ -1,4 +1,4 @@
-import ButtonCounter from './components/ButtonCounter';
+{/*import ButtonCounter from './components/ButtonCounter';
 import ColorBox from './components/ColorBox';
 import HelloWorld from './components/HelloWorld';
 import ShowDate from './components/ShowDate';
@@ -18,10 +18,24 @@ import ParentButtonOne from './props/ParentButtonOne';
 import ParentInputNotifier from './props/ParentInputNotifier';
 
 
+import HoverFont from './useState/Hoverfont';
+import AccessWithLimit from './useState/AccessWithLimit';
+import LoginWithLimit from './useState/LoginWithLimit';
+import DocumentTitleChanger from './useState/DocumentTitleChanger';
+import PostLikes from './useState/PostLikes';
+import CheckboxSummary from './useState/CheckboxSummary';
+import LanguageSwitcher from './useState/LanguageSwitcher';*/}
+import MultiSwitch from './useState/MultiSwitch';
+
+
+
+
+//const userData = { name: "Carlos", age: 30 };
+
 
 function App() {
   return (
-    <>
+    <>{/*
       <HelloWorld />
       <WelcomeUser />
       <ButtonCounter />
@@ -33,12 +47,23 @@ function App() {
       <EmojiList />
       <SimpleForm />
       <SumaForm />
+
       <Greeting name="Santiago" />
       <Greeting name="Maria" />
       <PromedioTresNotasForm />
       <UserCard user={userData} />
       <ParentButtonOne />
-      <ParentInputNotifier />
+      {/* <ParentInputNotifier /> 
+
+      <HoverFont />
+      <AccessWithLimit />
+      <LoginWithLimit />
+      <DocumentTitleChanger />
+      <PostLikes />
+      <CheckboxSummary />
+      <LanguageSwitcher />*/}
+      <MultiSwitch />
+      
     </>
   );
 }
